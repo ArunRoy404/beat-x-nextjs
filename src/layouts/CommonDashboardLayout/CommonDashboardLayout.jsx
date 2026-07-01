825668bef510920c39012e5e4cb2deb61e0651e6
ArunRoy404 <arunroy0065@gmail.com>
2026-07-01 23:00:32 +0600

    feat: add CommonDashboardLayout and CommonDashboardOutlet components for sidebar-integrated dashboard scaffolding

--- src/layouts/CommonDashboardLayout/CommonDashboardLayout.jsx
+++ src/layouts/CommonDashboardLayout/CommonDashboardLayout.jsx
@@ -5,9 +5,12 @@ import CommonDashboardSidebar from "./CommonDashboardSidebar"
 
 export default function CommonDashboardLayout({ children, sidebarData, sidebarTitle }) {
     return (
-        <SidebarProvider>
+        <SidebarProvider
+            className="bg-cover bg-center bg-no-repeat w-full min-h-screen"
+            style={{ backgroundImage: "url('/bg-images/dashboard_bg.png')" }}
+        >
             <CommonDashboardSidebar data={sidebarData} title={sidebarTitle} />
-            <SidebarInset>
+            <SidebarInset className="bg-transparent">
                 <CommonDashboardNavbar />
                 <CommonDashboardOutlet>{children}</CommonDashboardOutlet>
             </SidebarInset>
