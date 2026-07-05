40408f5f6d27c56ffa8397704b9017cf5325cf15
ArunRoy404 <arunroy0065@gmail.com>
2026-07-06 00:55:50 +0600

    feat: add Dialog and Tooltip UI components using @base-ui/react

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -16,9 +16,14 @@ function Dialog({
 }
 
 function DialogTrigger({
+  asChild,
+  children,
   ...props
 }) {
-  return <DialogPrimitive.Trigger data-slot="dialog-trigger" {...props} />;
+  if (asChild) {
+    return <DialogPrimitive.Trigger data-slot="dialog-trigger" render={children} {...props} />
+  }
+  return <DialogPrimitive.Trigger data-slot="dialog-trigger" {...props}>{children}</DialogPrimitive.Trigger>
 }
 
 function DialogPortal({
