928de52cfbb745f00fc074c1cf1eb94f31eba1c1
ArunRoy404 <arunroy0065@gmail.com>
2026-07-07 01:18:07 +0600

    feat: add CreateAlbumDialog and CreateAlbumForm components for admin dashboard album management

--- src/components/dialogs/admin/CreateAlbumDialog.jsx
+++ src/components/dialogs/admin/CreateAlbumDialog.jsx
@@ -2,41 +2,43 @@
 
 import React, { useState } from "react"
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { PlusCircle } from "lucide-react"
 import CreateAlbumForm from "@/components/forms/CreateAlbumForm"
 
 const CreateAlbumDialog = () => {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger asChild>
-                <Button variant="gradient" className="rounded-full cursor-pointer">
-                    <PlusCircle className="w-4 h-4 shrink-0" /> Create Album
-                </Button>
-            </DialogTrigger>
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="gradient">
+          <PlusCircle /> Create Album
+        </Button>
+      </DialogTrigger>
 
-            <DialogContent className="sm:max-w-[500px]">
-                {/* Custom Header */}
-                <DialogHeader>
-                    <DialogTitle>Create New Album</DialogTitle>
-                </DialogHeader>
+      <DialogContent>
+        {/* Custom Header */}
+        <DialogHeader>
+          <DialogTitle>
+            Create New Album
+          </DialogTitle>
+        </DialogHeader>
 
-                {/* Modular Form */}
-                <CreateAlbumForm
-                    onSuccess={() => setOpen(false)}
-                    onCancel={() => setOpen(false)}
-                />
-            </DialogContent>
-        </Dialog>
-    )
+        {/* New Modular Form */}
+        <CreateAlbumForm
+          onSuccess={() => setOpen(false)}
+          onCancel={() => setOpen(false)}
+        />
+      </DialogContent>
+    </Dialog>
+  )
 }
 
 export default CreateAlbumDialog
