a3c150758cf9ecc6158fc9df15b8eaed361fa1c4
ArunRoy404 <arunroy0065@gmail.com>
2026-08-04 00:49:43 +0600

    feat: implement admin shop dashboard with product management components and state management

---
+++ src/app/admin/dashboard/shop/page.jsx
@@ -0,0 +1,7 @@
+import AdminDashboardShopPage from "@/templates/admin/dashboard/AdminDashboardShopPage";
+
+const page = () => {
+  return <AdminDashboardShopPage />
+}
+
+export default page;
---
+++ src/templates/admin/dashboard/AdminDashboardShopPage.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React from "react"
+import { useAdminDashboardShopStore } from "@/zustandStore/admin/adminStore/adminDashboardShopStore"
+import DashboardStats from "@/components/shared/Dashboard/DashboardStats/DashboardStats"
+import AddNewProduct from "@/components/admin/shop/AddNewProduct"
+import ProductsContainer from "@/components/admin/shop/ProductsContainer"
+
+const AdminDashboardShopPage = () => {
+  const statsCards = useAdminDashboardShopStore((state) => state.shopStatsCards)
+
+  return (
+    <div className="flex flex-col gap-6 w-full pb-8">
+      {/* Upper Stats grid */}
+      <DashboardStats statsCards={statsCards} />
+
+      {/* Add New Product Banner */}
+      <AddNewProduct />
+
+      {/* Products list and filter container */}
+      <ProductsContainer />
+    </div>
+  )
+}
+
+export default AdminDashboardShopPage
